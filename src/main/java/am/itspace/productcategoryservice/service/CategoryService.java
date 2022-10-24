66a99d9ab08301d06bae08c862efc626f719ab4d
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-24 19:44:53 +0400

    CategoryService

--- src/main/java/am/itspace/productcategoryservice/service/CategoryService.java
+++ src/main/java/am/itspace/productcategoryservice/service/CategoryService.java
@@ -1,4 +1,20 @@
 package am.itspace.productcategoryservice.service;
 
-public interface ProductService {
+import am.itspace.productcategoryservice.dto.CategoryResponseDto;
+import am.itspace.productcategoryservice.dto.CreateCategoryDto;
+import am.itspace.productcategoryservice.dto.EditCategoryDto;
+
+import java.util.List;
+
+public interface CategoryService {
+    List<CategoryResponseDto> getAll();
+
+    CategoryResponseDto getById(int id);
+
+    void save(CreateCategoryDto createCategoryDto);
+
+    CategoryResponseDto update(int id, EditCategoryDto editCategoryDto);
+
+    void delete(int id);
 }
+
