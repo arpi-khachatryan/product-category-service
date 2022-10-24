1124c59b898ab1a277856e1308e3b7d1bf729fdd
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-24 19:50:34 +0400

    Product

--- src/main/java/am/itspace/productcategoryservice/model/Product.java
+++ src/main/java/am/itspace/productcategoryservice/model/Product.java
@@ -1,2 +1,24 @@
-package am.itspace.productcategoryservice.model;public class Product {
+package am.itspace.productcategoryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    private double price;
+    @ManyToOne
+    private Category category;
 }
