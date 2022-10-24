b34ad4be5fe0e9a9e02386c9250a74761f9073cd
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-24 19:46:18 +0400

    Error

--- src/main/java/am/itspace/productcategoryservice/exception/Error.java
+++ src/main/java/am/itspace/productcategoryservice/exception/Error.java
@@ -1,2 +1,20 @@
-package am.itspace.productcategoryservice.exception;public enum Error {
+package am.itspace.productcategoryservice.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum Error {
+
+    OBJECT_NOT_FOUND(4041, HttpStatus.NOT_FOUND, "Object not found"),
+
+    OBJECT_ALREADY_EXISTS(4042, HttpStatus.NOT_ACCEPTABLE, "Object already exists");
+
+    private final Integer code;
+    private final HttpStatus httpStatus;
+    private final String message;
 }
+
+
