6046e8e194e5b4b637d25479f9aa174efa421a06
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-24 19:43:05 +0400

    BaseException

--- src/main/java/am/itspace/productcategoryservice/exception/BaseException.java
+++ src/main/java/am/itspace/productcategoryservice/exception/BaseException.java
@@ -1,2 +1,19 @@
-package am.itspace.productcategoryservice.exception;public class BaseException {
+package am.itspace.productcategoryservice.exception;
+
+import lombok.Setter;
+import lombok.Getter;
+
+/**
+ * @author Arpi Khachatryan on 24.10.2022
+ */
+
+@Setter
+@Getter
+public class BaseException extends RuntimeException {
+
+    protected final Error error;
+
+    public BaseException(Error error) {
+        this.error = error;
+    }
 }
