43fe14d9e8c51e8d577ede5d7452eac5e32e2e78
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-24 19:47:04 +0400

    RestExceptionHandler

--- src/main/java/am/itspace/productcategoryservice/exception/RestExceptionHandler.java
+++ src/main/java/am/itspace/productcategoryservice/exception/RestExceptionHandler.java
@@ -1,21 +1,20 @@
-package com.polixis.task2.exception;
+package am.itspace.productcategoryservice.exception;
 
-import java.time.LocalDateTime;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.time.LocalDateTime;
+
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
-  @ExceptionHandler({BaseException.class})
-  public ResponseEntity<ApiError> handleIllegalArgument(BaseException ex) {
-    Error error = ex.getError();
-    ApiError apiError =
-        new ApiError(
-            error.getHttpStatus(), error.getCode(), LocalDateTime.now(), error.getMessage());
+    @ExceptionHandler({BaseException.class})
+    public ResponseEntity<ApiError> handleIllegalArgument(BaseException ex) {
+        Error error = ex.getError();
+        ApiError apiError = new ApiError(error.getHttpStatus(), error.getCode(), LocalDateTime.now(), error.getMessage());
 
-    return ResponseEntity.status(apiError.getStatus()).body(apiError);
-  }
+        return ResponseEntity.status(apiError.getStatus()).body(apiError);
+    }
 }
