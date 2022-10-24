ad19849538b2904ba8f4f1ae34ca3cafe728da22
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-24 19:51:14 +0400

    ProductEndpoint

--- src/main/java/am/itspace/productcategoryservice/controller/ProductEndpoint.java
+++ src/main/java/am/itspace/productcategoryservice/controller/ProductEndpoint.java
@@ -1,9 +1,48 @@
 package am.itspace.productcategoryservice.controller;
 
+import am.itspace.productcategoryservice.dto.CreateProductDto;
+import am.itspace.productcategoryservice.dto.EditProductDto;
+import am.itspace.productcategoryservice.dto.ProductResponseDto;
+import am.itspace.productcategoryservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-public class CategoryEndpoint {
+@RequestMapping("/products")
+public class ProductEndpoint {
+
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody CreateProductDto createProductDto) {
+        productService.save(createProductDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDto>> getAll() {
+        return ResponseEntity.ok(productService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> getById(@PathVariable("id") int id) {
+        return ResponseEntity.ok(productService.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductResponseDto> update(@PathVariable("id") int id,
+                                                     @RequestBody EditProductDto editProductDto) {
+        return ResponseEntity.ok(productService.update(id, editProductDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        productService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
