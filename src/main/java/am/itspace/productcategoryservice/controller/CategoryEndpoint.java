df1d331c5ac96858ceef5792b348ec72f0188fa9
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-24 19:43:32 +0400

    CategoryEndpoint

--- src/main/java/am/itspace/productcategoryservice/controller/CategoryEndpoint.java
+++ src/main/java/am/itspace/productcategoryservice/controller/CategoryEndpoint.java
@@ -1,2 +1,55 @@
-package am.itspace.productcategoryservice.controller;public class CategoryEndpoint {
+package am.itspace.productcategoryservice.controller;
+
+import am.itspace.productcategoryservice.dto.CategoryResponseDto;
+import am.itspace.productcategoryservice.dto.CreateCategoryDto;
+import am.itspace.productcategoryservice.dto.EditCategoryDto;
+import am.itspace.productcategoryservice.dto.ProductResponseDto;
+import am.itspace.productcategoryservice.service.CategoryService;
+import am.itspace.productcategoryservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/categories")
+public class CategoryEndpoint {
+
+    private final CategoryService categoryService;
+    private final ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody CreateCategoryDto createCategoryDto) {
+        categoryService.save(createCategoryDto);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoryResponseDto>> getAll() {
+        return ResponseEntity.ok(categoryService.getAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoryResponseDto> getById(@PathVariable("id") int id) {
+        return ResponseEntity.ok(categoryService.getById(id));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponseDto> update(@PathVariable("id") int id,
+                                                      @RequestBody EditCategoryDto editCategoryDto) {
+        return ResponseEntity.ok(categoryService.update(id, editCategoryDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") int id) {
+        categoryService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/byCategory/{id}")
+    public ResponseEntity<List<ProductResponseDto>> getAllProducts(@PathVariable("id") int id) {
+        return ResponseEntity.ok(productService.getByCategory(id));
+    }
 }
