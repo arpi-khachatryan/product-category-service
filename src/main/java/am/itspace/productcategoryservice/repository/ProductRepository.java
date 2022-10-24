e93eb17e3b3b124a819629a94255ef0fb7d4e3bc
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-25 01:00:34 +0400

    ProductRepository

--- src/main/java/am/itspace/productcategoryservice/repository/ProductRepository.java
+++ src/main/java/am/itspace/productcategoryservice/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package am.itspace.productcategoryservice.repository;
 
+import am.itspace.productcategoryservice.model.Category;
 import am.itspace.productcategoryservice.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,4 +9,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
     List<Product> findAllByCategoryId(Integer integer);
+
+    boolean existsByTitleAndPriceAndCategory(String title, double price, Category category);
 }
