c8ad93e2cf9cebdbb79471c03d164d5028d209d8
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-24 19:42:52 +0400

    ApiError

--- src/main/java/am/itspace/productcategoryservice/exception/ApiError.java
+++ src/main/java/am/itspace/productcategoryservice/exception/ApiError.java
@@ -1,31 +1,30 @@
-package com.polixis.task2.exception;
+package am.itspace.productcategoryservice.exception;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
-/**
- * @author Hovhannes Gevorgyan on 12.10.2022
- */
+import java.time.LocalDateTime;
+
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class ApiError {
 
-  @Schema(description = "Http response status", example = "CONFLICT")
-  private HttpStatus status;
+    @Schema(description = "Http response status", example = "CONFLICT")
+    private HttpStatus status;
 
-  @Schema(description = "The exception description", example = "4121")
-  private Integer errorCode;
+    @Schema(description = "The exception description", example = "4121")
+    private Integer errorCode;
 
-  @Schema(description = "Timestamp showing when the error occurred")
-  private LocalDateTime timestamp;
+    @Schema(description = "Timestamp showing when the error occurred")
+    private LocalDateTime timestamp;
 
-  @Schema(description = "Error message", example = "Directory not found")
-  private String message;
+    @Schema(description = "Error message", example = "Object not found")
+    private String message;
 }
