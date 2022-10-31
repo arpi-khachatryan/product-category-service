099af6d877c581a84b53ac1117306ca25ecc7059
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-31 16:21:30 +0400

    ProductServiceImpl

--- src/main/java/am/itspace/productcategoryservice/service/impl/ProductServiceImpl.java
+++ src/main/java/am/itspace/productcategoryservice/service/impl/ProductServiceImpl.java
@@ -3,8 +3,9 @@ package am.itspace.productcategoryservice.service.impl;
 import am.itspace.productcategoryservice.dto.CreateProductDto;
 import am.itspace.productcategoryservice.dto.EditProductDto;
 import am.itspace.productcategoryservice.dto.ProductResponseDto;
-import am.itspace.productcategoryservice.exception.*;
 import am.itspace.productcategoryservice.exception.Error;
+import am.itspace.productcategoryservice.exception.ProductAlreadyExistsException;
+import am.itspace.productcategoryservice.exception.ProductNotFoundException;
 import am.itspace.productcategoryservice.mapper.ProductMapper;
 import am.itspace.productcategoryservice.model.Product;
 import am.itspace.productcategoryservice.repository.ProductRepository;
@@ -12,13 +13,14 @@ import am.itspace.productcategoryservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 
 @Slf4j
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
 
@@ -37,31 +39,22 @@ public class ProductServiceImpl implements ProductService {
             log.info("Product with that title and price already exists");
             throw new ProductAlreadyExistsException(Error.PRODUCT_ALREADY_EXISTS);
         }
-        Product product = productMapper.mapToEntity(createProductDto);
-        productRepository.save(product);
         log.info("The Product was successfully stored in the database {}", createProductDto.getTitle());
-        return productMapper.mapToResponseDto(productRepository.getReferenceById(product.getId()));
+        return productMapper.mapToResponseDto(productRepository.save(productMapper.mapToEntity(createProductDto)));
     }
 
     @Override
     public ProductResponseDto getById(int id) {
-        Optional<Product> productOptional = productRepository.findById(id);
-        if (productOptional.isEmpty()) {
-            log.info("Product not found");
-            throw new ProductNotFoundException(Error.PRODUCT_NOT_FOUND);
-        }
-        log.info("Product successfully found {}", productOptional.get().getTitle());
-        return productMapper.mapToResponseDto(productOptional.get());
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new ProductNotFoundException(Error.PRODUCT_NOT_FOUND));
+        log.info("Product successfully found {}", product.getTitle());
+        return productMapper.mapToResponseDto(product);
     }
 
     @Override
     public ProductResponseDto update(int id, EditProductDto editProductDto) {
-        Optional<Product> productOptional = productRepository.findById(id);
-        if (productOptional.isEmpty()) {
-            log.info("Product not found");
-            throw new ProductNotFoundException(Error.PRODUCT_NOT_FOUND);
-        }
-        Product product = productOptional.get();
+        Product product = productRepository.findById(id).orElseThrow(
+                () -> new ProductNotFoundException(Error.PRODUCT_NOT_FOUND));
         if (editProductDto.getTitle() != null) {
             product.setTitle(editProductDto.getTitle());
         }
@@ -71,7 +64,6 @@ public class ProductServiceImpl implements ProductService {
         if (editProductDto.getCategory() != null) {
             product.setCategory(editProductDto.getCategory());
         }
-        productRepository.save(product);
         log.info("The Product was successfully stored in the database {}", product.getTitle());
         return productMapper.mapToResponseDto(product);
     }
@@ -88,12 +80,12 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public List<ProductResponseDto> getByCategory(int id) {
-        List<Product> allCategories = productRepository.findAllByCategoryId(id);
-        if (allCategories.isEmpty()) {
+        List<Product> products = productRepository.findAllByCategoryId(id);
+        if (products == null || products.isEmpty()) {
             log.info("Product not found");
             throw new ProductNotFoundException(Error.PRODUCT_NOT_FOUND);
         }
         log.info("Product successfully detected");
-        return productMapper.mapToResponseDtoList(allCategories);
+        return productMapper.mapToResponseDtoList(products);
     }
 }
