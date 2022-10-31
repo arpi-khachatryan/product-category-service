07251913ec514a6888ecc3786beb991baca029bb
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-31 16:24:35 +0400

    CurrentUserDetailServiceImpl

---
+++ src/main/java/am/itspace/productcategoryservice/security/CurrentUserDetailServiceImpl.java
@@ -0,0 +1,27 @@
+package am.itspace.productcategoryservice.security;
+
+import am.itspace.productcategoryservice.model.User;
+import am.itspace.productcategoryservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class CurrentUserDetailServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(username);
+        if (user.isEmpty()) {
+            throw new UsernameNotFoundException("Username not found");
+        }
+        return new CurrentUser(user.get());
+    }
+}
