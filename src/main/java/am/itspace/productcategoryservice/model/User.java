b70848983875f312ef5571dcc39a48900f3e2c99
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-31 16:20:56 +0400

    User

---
+++ src/main/java/am/itspace/productcategoryservice/model/User.java
@@ -0,0 +1,27 @@
+package am.itspace.productcategoryservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
