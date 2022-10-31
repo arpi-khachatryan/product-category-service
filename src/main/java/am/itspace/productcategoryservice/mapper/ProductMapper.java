dd7cdaf8ade7f34d7021c3b59f7477213d4846ef
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-31 16:21:51 +0400

    ProductMapper

--- src/main/java/am/itspace/productcategoryservice/mapper/ProductMapper.java
+++ src/main/java/am/itspace/productcategoryservice/mapper/ProductMapper.java
@@ -1,7 +1,6 @@
 package am.itspace.productcategoryservice.mapper;
 
 import am.itspace.productcategoryservice.dto.CreateProductDto;
-import am.itspace.productcategoryservice.dto.EditProductDto;
 import am.itspace.productcategoryservice.dto.ProductResponseDto;
 import am.itspace.productcategoryservice.model.Product;
 import org.mapstruct.Mapper;
@@ -12,8 +11,6 @@ import java.util.List;
 public interface ProductMapper {
 
     Product mapToEntity(CreateProductDto createProductDto);
-
-    Product mapToEntity(EditProductDto editProductDto);
 
     ProductResponseDto mapToResponseDto(Product product);
 
