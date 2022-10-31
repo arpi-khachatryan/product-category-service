026740b7bf0b1dd5f8e4e45ebfc97666fd62d226
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-31 16:20:31 +0400

    UserAuthResponceDto

---
+++ src/main/java/am/itspace/productcategoryservice/dto/UserAuthResponceDto.java
@@ -0,0 +1,16 @@
+package am.itspace.productcategoryservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserAuthResponceDto {
+
+    private String token;
+    private UserResponceDto user;
+}
