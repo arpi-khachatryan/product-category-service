72d9cf849569ffcf2c6b74304bb5b2de1af9922b
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-31 16:23:54 +0400

    Error

--- src/main/java/am/itspace/productcategoryservice/exception/Error.java
+++ src/main/java/am/itspace/productcategoryservice/exception/Error.java
@@ -12,9 +12,13 @@ public enum Error {
 
     CATEGORY_ALREADY_EXISTS(4001, HttpStatus.BAD_REQUEST, "Category already exists"),
 
-    PRODUCT_NOT_FOUND(4041, HttpStatus.NOT_FOUND, "Product not found"),
+    PRODUCT_NOT_FOUND(4042, HttpStatus.NOT_FOUND, "Product not found"),
 
-    PRODUCT_ALREADY_EXISTS(4001, HttpStatus.BAD_REQUEST, "Product already exists");
+    PRODUCT_ALREADY_EXISTS(4002, HttpStatus.BAD_REQUEST, "Product already exists"),
+
+    USER_REGISTRATION_FAILED(4091, HttpStatus.CONFLICT, "Email already exists"),
+
+    USER_NOT_FOUND(4011, HttpStatus.UNAUTHORIZED, "User not found");
 
     private final Integer code;
     private final HttpStatus httpStatus;
