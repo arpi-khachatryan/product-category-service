bd9307e4d8d176067f08a193f34e25cefb713277
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-10-31 16:19:43 +0400

    UserRepository

---
+++ src/main/java/am/itspace/productcategoryservice/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package am.itspace.productcategoryservice.repository;
+
+import am.itspace.productcategoryservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String username);
+
+    boolean existsByEmail(String email);
+}
