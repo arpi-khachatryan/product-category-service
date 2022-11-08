5a0c61d7d35094872e15440a84c8afb184c9584c
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-11-08 17:31:42 +0400

    ProductCategoryServiceApplication

--- src/main/java/am/itspace/productcategoryservice/ProductCategoryServiceApplication.java
+++ src/main/java/am/itspace/productcategoryservice/ProductCategoryServiceApplication.java
@@ -2,11 +2,21 @@ package am.itspace.productcategoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+//@EnableSwagger2
+@EnableScheduling
 public class ProductCategoryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ProductCategoryServiceApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
