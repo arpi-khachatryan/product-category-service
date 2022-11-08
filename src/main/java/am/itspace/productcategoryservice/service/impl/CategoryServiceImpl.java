912217d834bee6be742c4ff2dafbfb7205bdd3d0
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-11-08 17:23:08 +0400

    CategoryServiceImpl

--- src/main/java/am/itspace/productcategoryservice/service/impl/CategoryServiceImpl.java
+++ src/main/java/am/itspace/productcategoryservice/service/impl/CategoryServiceImpl.java
@@ -9,6 +9,7 @@ import am.itspace.productcategoryservice.exception.Error;
 import am.itspace.productcategoryservice.mapper.CategoryMapper;
 import am.itspace.productcategoryservice.model.Category;
 import am.itspace.productcategoryservice.repository.CategoryRepository;
+import am.itspace.productcategoryservice.security.CurrentUser;
 import am.itspace.productcategoryservice.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +17,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -25,6 +27,16 @@ public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
+
+    @Override
+    public CategoryResponseDto save(CreateCategoryDto createCategoryDto) {
+        if (categoryRepository.existsByName(createCategoryDto.getName())) {
+            log.info("Category with that name already exists {}", createCategoryDto.getName());
+            throw new CategoryAlreadyExistsException(Error.CATEGORY_ALREADY_EXISTS);
+        }
+        log.info("The Category was successfully stored in the database {}", createCategoryDto.getName());
+        return categoryMapper.mapToResponseDto(categoryRepository.save(categoryMapper.mapToEntity(createCategoryDto)));
+    }
 
     @Override
     public List<CategoryResponseDto> getAll() {
@@ -40,21 +52,12 @@ public class CategoryServiceImpl implements CategoryService {
     }
 
     @Override
-    public CategoryResponseDto save(CreateCategoryDto createCategoryDto) {
-        if (categoryRepository.existsByName(createCategoryDto.getName())) {
-            log.info("Category with that name already exists {}", createCategoryDto.getName());
-            throw new CategoryAlreadyExistsException(Error.CATEGORY_ALREADY_EXISTS);
-        }
-        log.info("The Category was successfully stored in the database {}", createCategoryDto.getName());
-        return categoryMapper.mapToResponseDto(categoryRepository.save(categoryMapper.mapToEntity(createCategoryDto)));
-    }
-
-    @Override
     public CategoryResponseDto update(int id, EditCategoryDto editCategoryDto) {
         Category category = categoryRepository.findById(id).orElseThrow(() -> new CategoryNotFoundException(Error.CATEGORY_NOT_FOUND));
         log.info("Category with that id not found");
         if (editCategoryDto.getName() != null) {
             category.setName(editCategoryDto.getName());
+            categoryRepository.save(category);
         }
         log.info("The Category was successfully stored in the database {}", category.getName());
         return categoryMapper.mapToResponseDto(category);
@@ -62,11 +65,11 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public void delete(int id) {
-        if (!categoryRepository.existsById(id)) {
-            log.info("Category not found");
-            throw new CategoryNotFoundException(Error.CATEGORY_NOT_FOUND);
+        if (categoryRepository.existsById(id)) {
+            categoryRepository.deleteById(id);
+            log.info("The category has been successfully deleted");
         }
-        categoryRepository.deleteById(id);
-        log.info("The category has been successfully deleted");
+        log.info("Category not found");
+        throw new CategoryNotFoundException(Error.CATEGORY_NOT_FOUND);
     }
 }
