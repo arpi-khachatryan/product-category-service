4874a3a015be1a10a52d407b0ad1399d8e287c61
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-11-08 17:28:57 +0400

    ProductServiceImpl

--- src/main/java/am/itspace/productcategoryservice/service/impl/ProductServiceImpl.java
+++ src/main/java/am/itspace/productcategoryservice/service/impl/ProductServiceImpl.java
@@ -12,9 +12,12 @@ import am.itspace.productcategoryservice.repository.ProductRepository;
 import am.itspace.productcategoryservice.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -26,11 +29,31 @@ public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
     private final ProductMapper productMapper;
+    private final RestTemplate restTemplate;
+
 
     @Override
     public List<ProductResponseDto> getAll() {
+        List<Product> products = productRepository.findAll();
+        if (!products.isEmpty()) {
+            ResponseEntity<HashMap> currency = restTemplate.getForEntity("https://cb.am/latest.json.php?currency=USD",
+                    HashMap.class);
+            HashMap<String, String> hashMap = currency.getBody();
+            if (!hashMap.isEmpty()) {
+                double usdCurrency = Double.parseDouble(hashMap.get("USD"));
+                if (usdCurrency > 0) {
+                    for (Product product : products) {
+                        product.setPrice(product.getPrice() / usdCurrency);
+//                        double price = product.getPrice() / usdCurrency;
+//                        DecimalFormat df = new DecimalFormat("#.##");
+//                        Double aDouble = Double.valueOf(df.format(price));
+//                        product.setPrice(aDouble);
+                    }
+                }
+            }
+        }
         log.info("Product successfully detected");
-        return productMapper.mapToResponseDtoList(productRepository.findAll());
+        return productMapper.mapToResponseDtoList(products);
     }
 
     @Override
@@ -64,24 +87,25 @@ public class ProductServiceImpl implements ProductService {
         if (editProductDto.getCategory() != null) {
             product.setCategory(editProductDto.getCategory());
         }
+        productRepository.save(product);
         log.info("The Product was successfully stored in the database {}", product.getTitle());
         return productMapper.mapToResponseDto(product);
     }
 
     @Override
     public void delete(int id) {
-        if (!productRepository.existsById(id)) {
-            log.info("Product not found");
-            throw new ProductNotFoundException(Error.PRODUCT_NOT_FOUND);
+        if (productRepository.existsById(id)) {
+            productRepository.deleteById(id);
+            log.info("The product has been successfully deleted");
         }
-        productRepository.deleteById(id);
-        log.info("The product has been successfully deleted");
+        log.info("Product not found");
+        throw new ProductNotFoundException(Error.PRODUCT_NOT_FOUND);
     }
 
     @Override
     public List<ProductResponseDto> getByCategory(int id) {
         List<Product> products = productRepository.findAllByCategoryId(id);
-        if (products == null || products.isEmpty()) {
+        if (products == null) {
             log.info("Product not found");
             throw new ProductNotFoundException(Error.PRODUCT_NOT_FOUND);
         }
